3edd430cd946f3ed22fe84f1b2158f74055fb281
Dov <dovindustries@gmail.com>
2025-02-24 12:26:38 -0500

    Fixed linting error.

--- charitywallet/app/layout.tsx
+++ charitywallet/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Footer from "./footer";
 import { ThirdwebProvider } from "thirdweb/react";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/react";
