7cd93220c2cbc40590f473c4cdf8ee4d508dabac
Dov <dovindustries@gmail.com>
2025-03-11 15:32:24 -0400

    replaced address line with registrered office address.

---
+++ charitywallet/prisma/migrations/20250311193149_replaced_address_line_with_registered_office_address/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `addressLine` on the `charity` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "charity" DROP COLUMN "addressLine";
