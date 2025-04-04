f34583a58beb441191c4705a4a161ba092abe8e6
Dov <dovindustries@gmail.com>
2025-04-04 14:28:03 -0400

    changed variable to erc20

--- charitywallet/hooks/use-send-with-fee.tsx
+++ charitywallet/hooks/use-send-with-fee.tsx
@@ -6,7 +6,7 @@ import { toast } from "@/hooks/use-toast";
 import { client } from "@/lib/thirdwebClient";
 import { polygon } from "thirdweb/chains";
 
-const USDC_POLYGON_MAINNET_ADDRESS =
+const ERC20_POLYGON_MAINNET_ADDRESS =
   "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359";
 const CUSTOM_CONTRACT_ADDRESS = "0x7C80328071C13026A299561d19042004ab899c4f";
 
@@ -25,7 +25,7 @@ export function useSendWithFee(
       // STEP 1: Approve the contract to spend the donation tokens.
       const usdcContract = getContract({
         client,
-        address: USDC_POLYGON_MAINNET_ADDRESS,
+        address: ERC20_POLYGON_MAINNET_ADDRESS,
         chain: polygon,
       });
 
@@ -76,7 +76,11 @@ export function useSendWithFee(
       const donationTransaction = prepareContractCall({
         contract: customContract,
         method: "function sendWithFeeToken(uint256,address,address)",
-        params: [donationValue, recipientAddress, USDC_POLYGON_MAINNET_ADDRESS],
+        params: [
+          donationValue,
+          recipientAddress,
+          ERC20_POLYGON_MAINNET_ADDRESS,
+        ],
       });
 
       sendTx(donationTransaction, {
