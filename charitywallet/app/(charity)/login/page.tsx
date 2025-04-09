70437f2a2b92ddd6a938869e216e56a73f6a6c02
Dov <dovindustries@gmail.com>
2025-04-09 11:34:06 -0400

    Removed log in title from login page.

--- charitywallet/app/(charity)/login/page.tsx
+++ charitywallet/app/(charity)/login/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
         <ConnectEmbed
           client={client}
           wallets={wallets}
-          header={{ title: "" }}
+          header={{ title: " " }}
           showThirdwebBranding={false}
           chain={ethereum}
           auth={{
