2a32aeef7577956d0fbdafd9a65ce30c38cb07b9
Dov <dovindustries@gmail.com>
2025-04-23 10:54:40 -0400

    Improved story.

--- charitywallet/components/donation-form/explorer-link.stories.tsx
+++ charitywallet/components/donation-form/explorer-link.stories.tsx
@@ -1,18 +1,26 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { ExplorerLink } from "./explorer-link";
 
-export default {
+const meta: Meta<typeof ExplorerLink> = {
   title: "DonationForm/ExplorerLink",
   component: ExplorerLink,
+  render: (args) => <ExplorerLink {...args} />,
 };
 
-export const Empty = {
+export default meta;
+
+type Story = StoryObj<typeof ExplorerLink>;
+
+export const Empty: Story = {
   args: {
     txHash: "",
   },
 };
 
-export const WithHash = {
+export const WithHash: Story = {
   args: {
     txHash: "0xDEADBEEF0123456789abcdef",
   },
+  decorators: [(Story) => <div style={{ padding: '3em' }}><Story /></div>],
 };
